01224cfacb82f458938a759d3d20c951dd3eb11a
Gilberto Suarez <gsuarez@acid.cl>
2019-12-13 12:06:07 -0300

    initial configuration nextjs

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Head from "next/head";
+import Nav from "../components/nav";
+
+const Home = () => {
+  return <div>Welcome to Market-Place!</div>;
+};
+
+export default Home;
