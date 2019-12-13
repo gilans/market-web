497800e22fc4f7f1f52434e04c3adf64a5a33942
Gilberto Suarez <gsuarez@acid.cl>
2019-12-13 12:20:47 -0300

    adding sass configuration

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Head from "next/head";
 import Nav from "../components/nav";
-
+import "./styles/Home.scss";
 const Home = () => {
-  return <div>Welcome to Market-Place!</div>;
+  return <div className="example">Welcome to Market-Place!</div>;
 };
 
 export default Home;
