5021f634c53f2e4e510cfe9cc52c011264dc7d8f
Gilberto Suarez <gilbertosuarez@pop-os.localdomain>
2019-12-15 07:56:24 -0300

    settings of prettier and eslint

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import React from "react";
-import Head from "next/head";
-import Nav from "../components/nav";
-import "./styles/Home.scss";
+import React from 'react';
+import Head from 'next/head';
+import Nav from '../components/nav';
+import './styles/Home.scss';
 const Home = () => {
   return <div className="example">Welcome to Market-Place!</div>;
 };
