34cc7f9f322a90792c29bb99119b7eab4b0cb62d
Gilberto Suarez <gilbertosuarez@pop-os.localdomain>
2019-12-15 21:36:17 -0300

    delete nav component

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import HomeHeader from '../src/containers/HomeHeader/HomeHeader';
 
-import Nav from '../components/nav';
 import './styles/Home.scss';
 const Home = () => {
   return (
